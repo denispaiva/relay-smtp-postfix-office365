0e0d9fdab0b1703f414d180605254d995d97b4fe
Denis Paiva <denispaiva@gmail.com>
2018-10-09 16:17:11 -0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/env ash
+set -e # exit on error
+
+# Variables
+if [ -z "$SMTP_LOGIN" -o -z "$SMTP_PASSWORD" ] ; then
+	echo "SMTP_LOGIN and SMTP_PASSWORD _must_ be defined"
+	exit 1
+fi
+export SMTP_LOGIN SMTP_PASSWORD
+export EXT_RELAY_HOST=${EXT_RELAY_HOST:-"smtp.office365.com"}
+export EXT_RELAY_PORT=${EXT_RELAY_PORT:-"587"}
+export RELAY_HOST_NAME=${RELAY_HOST_NAME:-"relay.example.com"}
+export ACCEPTED_NETWORKS=${ACCEPTED_NETWORKS:-"192.168.0.0/16 172.16.0.0/12 10.0.0.0/8"}
+export USE_TLS=${USE_TLS:-"yes"}
+export TLS_VERIFY=${TLS_VERIFY:-"may"}
+
+echo $RELAY_HOST_NAME > /etc/mailname
+
+# Templates
+j2 /root/conf/postfix-main.cf > /etc/postfix/main.cf
+j2 /root/conf/sasl_passwd > /etc/postfix/sasl_passwd
+postmap /etc/postfix/sasl_passwd
+
+# Launch
+rm -f /var/spool/postfix/pid/*.pid
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
